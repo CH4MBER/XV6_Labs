fc9c7e06a3520bcefa64a5624b6aff76f6f3abfc
CH4MBER <1104807401@qq.com>
2024-07-31 03:27:01 +0800

    lab6 finished

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -8,6 +8,8 @@
 #define NBUCKET 5
 #define NKEYS 100000
 
+pthread_mutex_t lock[NBUCKET];
+
 struct entry {
   int key;
   int value;
@@ -52,7 +54,9 @@ void put(int key, int value)
     e->value = value;
   } else {
     // the new is new.
+    pthread_mutex_lock(&lock[i]);
     insert(key, value, &table[i], table[i]);
+    pthread_mutex_unlock(&lock[i]);
   }
 
 }
@@ -105,6 +109,7 @@ main(int argc, char *argv[])
   void *value;
   double t1, t0;
 
+  
 
   if (argc < 2) {
     fprintf(stderr, "Usage: %s nthreads\n", argv[0]);
@@ -117,7 +122,10 @@ main(int argc, char *argv[])
   for (int i = 0; i < NKEYS; i++) {
     keys[i] = random();
   }
-
+  
+  for (int i = 0; i < NBUCKET; i++){      
+    pthread_mutex_init(&lock[i], NULL);
+  } 
   //
   // first the puts
   //
